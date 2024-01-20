1ef160fb2e061fd95024ccab819d978225176f17
Douglas Chimento <dchimento@gmail.com>
2024-01-20 22:16:31 +0200

    update rpc_url_from_cluster and dev cluster urls

--- src/util.rs
+++ src/util.rs
@@ -2,7 +2,7 @@ use crate::common::Cluster;
 
 pub fn rpc_url_from_cluster(api_key: String, cluster: Cluster) -> String {
     return match cluster {
-        Cluster::MainnetBeta => format!("https://rpc.helius.xyz/?api-key={api_key}"),
-        Cluster::Devnet => format!("https://rpc-devnet.helius.xyz/?api-key={api_key}"),
+        Cluster::MainnetBeta => format!("https://devnet.helius-rpc.com/?api-key={api_key}"),
+        Cluster::Devnet => format!("https://mainnet.helius-rpc.com/?api-key={api_key}"),
     };
 }
