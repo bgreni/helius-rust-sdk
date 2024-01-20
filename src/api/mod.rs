fff56f674d0cacf329c8033a19658ca2cb5636e6
Douglas Chimento <dchimento@gmail.com>
2024-01-20 22:16:31 +0200

    update rpc_url_from_cluster and dev cluster urls

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -19,7 +19,7 @@ use crate::request_handler::RequestHandler;
 
 const API_URL_V1: &str = "https://api-mainnet.helius-rpc.com/v1";
 const API_URL_V0: &str = "https://api-mainnet.helius-rpc.com/v0";
-const DEV_API_URL_V0: &str = "https://api-devnet.helius.xyz/v0";
+const DEV_API_URL_V0: &str = "https://api-devnet.helius-rpc.com/v0";
 const DAS_URL: &str = "https://mainnet.helius-rpc.com";
 
 pub struct Helius {
